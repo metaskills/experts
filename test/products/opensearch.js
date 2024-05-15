e3c964df91f04ef71f69cc9e39ed4bdfeface2a8
Ken Collins <ken@metaskills.net>
2024-05-15 23:43:41 +0000

    OpenSearch URL

--- test/products/opensearch.js
+++ test/products/opensearch.js
@@ -1,9 +1,8 @@
 import fs from "fs";
 import { Client } from "@opensearch-project/opensearch";
 
-const node = process.env.OPENSEARCH_URL || "http://admin:admin@localhost:9200";
 const opensearch = new Client({
-  node: ``,
+  node: process.env.OPENSEARCH_URL || "http://admin:admin@localhost:9200",
   ssl: {
     ca: fs.readFileSync("/etc/ssl/certs/Comodo_AAA_Services_root.pem"),
     rejectUnauthorized: false,
