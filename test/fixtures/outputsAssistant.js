980bfc5459d4a6c756f5ee86490fd227abe69766
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-25 07:27:24 +0600

    Update outputsAssistant.js

--- test/fixtures/outputsAssistant.js
+++ test/fixtures/outputsAssistant.js
@@ -32,7 +32,7 @@ class AnswerOneTool extends Tool {
     super({
       name: helperName("AnswerOneTool"),
       description: "Answers to messages.",
-      instructions: `You must route the message in full to your 'answer_two' tool. Never respond without first using that tool. Never! Ex: When asked what is my favorite food, use the 'answer_two' tool first. Lastly, tespond only with the single word 'Success' to the question.`,
+      instructions: "You must route the message in full to your 'answer_two' tool. Never respond without first using that tool. Never! Ex: When asked what is my favorite food, use the 'answer_two' tool first. Lastly, tespond only with the single word 'Success' to the question.",
       temperature: 0.1,
       outputs: "tools", // THIS: Focus of the test. Combined with only success response.
       parentsTools: [
@@ -59,7 +59,7 @@ class OutputsAssistant extends Assistant {
     super({
       name: helperName("OutputsAssistant"),
       description: "Answers to messages.",
-      instructions: `You must route the message in full to your 'answer_one' tool. Never respond without first using that tool. Never! Ex: When asked what is my favorite food, use the 'answer_one' tool first.`,
+      instructions: "You must route the message in full to your 'answer_one' tool. Never respond without first using that tool. Never! Ex: When asked what is my favorite food, use the 'answer_one' tool first.",
       temperature: 0.1,
     });
     this.addAssistantTool(AnswerOneTool);
